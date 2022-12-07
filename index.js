88c00b7c69c0f74edca6f540ad53e3f093f1d988
Alex Dauncey <alexanderdauncey@gmail.com>
2022-12-07 13:49:03 +0000

    base structure

---
+++ index.js
@@ -0,0 +1,20 @@
+var config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scene: {
+    preload: preload,
+    create: create,
+    update: update,
+  },
+  pixelArt true,
+}
+
+function preload() {
+}
+
+function create() {
+}
+
+function update() {
+}
