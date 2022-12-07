bd96f8c755c5ec1bbd7d5763e622a91ed6046131
Alex Dauncey <alexanderdauncey@gmail.com>
2022-12-07 15:39:17 +0000

    multiple scenes, global state

--- js/index.js
+++ js/index.js
@@ -1,50 +1,20 @@
 import { updateWeather } from "./src/weather.js";
+import ColdScene from './src/scenes/coldscene.js';
+import HotScene from './src/scenes/hotscene.js';
 
 var config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
-  scene: {
-    preload: preload,
-    create: create,
-    update: update,
-  },
+  physics: {
+    default: 'arcade',
+    arcade: {
+        debug: false,
+        pixelArt: true,
+    }
+},
+  scene: [ ColdScene, HotScene ],
   pixelArt: true,
 }
-
-let state = {
-  timer: 1000,
-  weather: "hot",
-  lastChange: 0,
-}
-
-let ui = {
-  weatherText: null,
-}
-
-function preload() {
-
-}
-
-function create() {
-  this.add.text(20, 20, "Current Weather: ", {
-    fontFamily: 'KenneyPixel',
-    fontSize: '40px',
-  });
-
-  ui.weatherText = this.add.text(400, 20, "", {
-    fontFamily: 'KenneyPixel',
-    fontSize: '40px',
-  });
-
-}
-
-function update() {
-  ui.weatherText.setText(state.weather);
-
-  updateWeather(this, state);
-
-}
-
 
 let game = new Phaser.Game(config);
